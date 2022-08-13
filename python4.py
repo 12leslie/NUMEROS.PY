504248272d3e0a2e6aec9bfeeebba2626cd9d49c
12leslie <106928251+12leslie@users.noreply.github.com>
2022-08-13 10:45:40 -0600

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,5 @@
+import math
+cateto1 = float(input("INGRESE EL CATETO A: "))
+cateto2 = float(input("INGRESE ELCATETO B: "))
+hipotenusa = math.sqrt((cateto1*2) + (cateto2*2))
+print("Hipotenusa: ", hipotenusa)
